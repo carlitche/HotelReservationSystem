651421743cf3ab113d0dc71ffd8f6fadde9fd667
carlitche <25907346+carlitche@users.noreply.github.com>
2023-05-01 18:48:48 +0200

    hotel service initial

---
+++ hotel-service/src/test/java/com/carlitche/hotelservice/HotelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlitche.hotelservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
