10a52c38625fcada50cf7494d2bb8c79a76abb0d
carlitche <25907346+carlitche@users.noreply.github.com>
2023-05-17 16:45:27 +0200

    following SonarLint recommendation

--- hotel-service/src/main/java/com/carlitche/hotelservice/HotelServiceApplication.java
+++ hotel-service/src/main/java/com/carlitche/hotelservice/HotelServiceApplication.java
@@ -3,14 +3,11 @@ package com.carlitche.hotelservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-
 @SpringBootApplication
 public class HotelServiceApplication {
 
 	public static void main(String[] args) {
-		var app = SpringApplication.run(HotelServiceApplication.class, args);
-//		Arrays.stream(app.getBeanDefinitionNames()).forEach(System.out::println);
+		SpringApplication.run(HotelServiceApplication.class, args);
 	}
 
 }
